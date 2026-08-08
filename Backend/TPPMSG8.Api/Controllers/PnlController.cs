82708d4fd9a5b78b1aa9a9ffd35d476767a51743
snavernekar <snavernekar@ivp.in>
2026-08-08 10:27:49 +0530

    added + tested PnL view API

---
+++ Backend/TPPMSG8.Api/Controllers/PnlController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPPMSG8.Application.Interfaces;
+
+namespace TPPMSG8.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PnlController : ControllerBase
+    {
+        private readonly IPnlService _pnlService;
+
+        public PnlController(IPnlService pnlService)
+        {
+            _pnlService = pnlService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPnl([FromQuery] DateTime valuationDate)
+        {
+            try
+            {
+                // Default to the case study required date if none is provided
+                if (valuationDate == default)
+                {
+                    valuationDate = new DateTime(2026, 3, 31);
+                }
+
+                var pnlData = await _pnlService.GetPnlAsOfDateAsync(valuationDate);
+                return Ok(pnlData);
+            }
+            catch (Exception ex)
+            {
+                // In a production app, log the exception here
+                return StatusCode(500, "An error occurred while calculating PnL.");
+            }
+        }
+    }
+}
